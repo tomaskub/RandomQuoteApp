bf5ca920b8505f63501cf806fec27cf61757532a
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-03-07 15:45:31 +0100

    Add quoteServiceType and QuoteService Add Quote struct conforming to Decodable

--- RandomQuote/ViewController.swift
+++ RandomQuote/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
 
@@ -21,3 +22,26 @@ class ViewController: UIViewController {
     
 }
 
+protocol QuoteServiceType {
+    func getRandomQuote() -> AnyPublisher<Quote, Error>
+}
+
+class QuoteService: QuoteServiceType {
+    func getRandomQuote() -> AnyPublisher<Quote, Error> {
+        let url = URL(string: "http://api.quotable.io/random")!
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .catch { error in
+                return Fail(error: error).eraseToAnyPublisher()
+            }
+            .map { $0.data }
+            .decode(type: Quote.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+        
+    }
+}
+
+struct Quote: Decodable {
+    let content: String
+    let author: String
+}
